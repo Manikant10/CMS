9dea755a25dc28d0eea7db05d845468a6713259f
Manikant10 <manikantraju123@gmail.com>
2026-03-21 16:49:15 +0530

    Fix ESLint error - Add missing useNavigate import

--- client/web/src/LoginDashboard.js
+++ client/web/src/LoginDashboard.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 import apiConfig from './config/api';
 import './LoginDashboard.css';
